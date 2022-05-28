76afaf63ed6598068972776fea4e7e7479e5d9d2
Moupiya-M67 <1905328@kiit.ac.in>
2022-05-28 15:59:43 +0530

    first commit

---
+++ src/components/Notes.js
@@ -0,0 +1,90 @@
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import AddNote from './AddNote';
+import NoteContext from '../context/notes/NoteContext'
+import NoteItem from './NoteItem';
+
+const Notes = (props) => {
+    const context = useContext(NoteContext);
+    let navigate = useNavigate();
+    const { notes, getNotes, editNote } = context;
+
+    const [note, setNote] = useState({ id: "", etitle: "", edescription: "", etag: "" })
+    const handleClick = (e) => {
+        editNote(note.id, note.etitle, note.edescription, note.etag);
+        refClose.current.click();
+        props.showAlert("Updated successfully","success");
+    }
+    const onChange = (e) => {
+        setNote({ ...note, [e.target.name]: e.target.value })
+    }
+    useEffect(() => {
+        if(localStorage.getItem('auth-token'))
+        {
+            getNotes();
+        }
+        else{
+            navigate("/login");
+        }
+        // eslint-disable-next-line
+    }, [])
+    const updateNote = (currentNote) => {
+        ref.current.click();
+        setNote({ id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag });
+        
+    }
+    const ref = useRef(null);
+    const refClose = useRef(null);
+    return (
+        <>
+            <AddNote showAlert={props.showAlert}/>
+            <button type="button" ref={ref} className="btn btn-primary d-none" data-toggle="modal" data-target="#exampleModal">
+                Launch demo modal
+            </button>
+            <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <form className="my-3">
+                                <div className="mb-3">
+                                    <label htmlFor="exampleInputEmail1" className="form-label">Title</label>
+                                    <input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} aria-describedby="emailHelp" onChange={onChange} minLength={5} required />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="exampleInputPassword1" className="form-label">Description</label>
+                                    <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} minLength={5} required />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="exampleInputPassword1" className="form-label">Tag</label>
+                                    <input type="text" className="form-control" id="etag" name="etag" value={note.etag} onChange={onChange} minLength={5} required />
+                                </div>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" ref={refClose} className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button disabled={note.etitle.length<5 || note.edescription.length<5} type="button" onClick={handleClick} className="btn btn-primary">Update Note</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row my-3 mx-1">
+                <h2>Yours Notes</h2>
+                <div className="container my-1">
+                    {notes.length===0 && 'No notes to display'}
+                </div>
+               
+                {notes.map((note) => {
+                    return <NoteItem showAlert={props.showAlert} key={note._id} updateNote={updateNote} note={note} />;
+                })}
+            </div>
+        </>
+    )
+}
+
+export default Notes;
